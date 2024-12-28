218ea8e004eee883d663dc42d66bc930062d3df9
zReBolTz <ferreiraw36@gmail.com>
2024-12-27 22:26:48 -0300

    feat: adicionando função para exclusão de filme

--- src/pages/Favoritos/Favoritos.jsx
+++ src/pages/Favoritos/Favoritos.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import './Style.css'
+import { toast } from "react-toastify";
 
 const Favoritos = () => {
     const [movies, setMovies]= useState([])
@@ -9,9 +10,19 @@ const Favoritos = () => {
         setMovies(JSON.parse(saveMovies) || [])
        
     },[])
+
+    function deleteMovies(id){
+        const newMovies= movies.filter((item)=>{
+           return (item.id !== id)
+        })
+        setMovies(newMovies)
+        localStorage.setItem('@primeFlix', JSON.stringify(newMovies))
+        toast.success('Filme excluído com sucesso!')
+    }
     return ( 
         <div className="my-movies">
             <h1>Meus Filmes</h1>
+            {movies.length== 0 && <span>você não possui filmes no momento </span>}
             <ul>
                 {movies.map((item)=>{
                     return(
@@ -19,7 +30,7 @@ const Favoritos = () => {
                             <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}> Ver Detalhes </Link>
-                                <button>Excluir</button>
+                                <button onClick={()=> deleteMovies(item.id)}>Excluir</button>
                             </div>
                         </li>
                     )
